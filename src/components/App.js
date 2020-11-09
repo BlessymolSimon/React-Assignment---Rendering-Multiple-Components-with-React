44eccbd2d431664093dd26f3608355d372f4afa9
Blessymol Simon <blessy4mooc@gmail.com>
2020-11-10 03:13:53 +0530

    1st edits as per qn

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <></>
+    <div>
+    <h1 data-ns-test="project-name">Memory game</h1>
+    <p data-ns-test="project-description">in this description i have to explain my dream project</p>
+    </div>
   )
 }
 
